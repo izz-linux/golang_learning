dee69756aea98ff81e0a7b74939d808a46faef26
Izz Noland <izz@shipt.com>
2021-07-15 05:27:58 -0500

    starting the first assignments

---
+++ 00-my_code/exercises/ninja_l1/01.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	x,y,z := 42,"James,Bond",true
+	fmt.Println(x, y, z)
+}
---
+++ 00-my_code/exercises/ninja_l1/02.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T", x)
+	fmt.Println(y)
+	fmt.Printf("%T", y)
+	fmt.Println(z)
+	fmt.Printf("%T", z)
+
+
+}
