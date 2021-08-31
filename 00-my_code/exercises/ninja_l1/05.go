756a3b8d1d1afb3e25528f0d7210b3acdf0404f0
Izz Noland <izz@shipt.com>
2021-08-31 15:54:12 -0500

    finished exercises and quiz

--- 00-my_code/exercises/ninja_l1/05.go
+++ 00-my_code/exercises/ninja_l1/05.go
@@ -4,11 +4,17 @@ import "fmt"
 
 type izzType int
 var x izzType
+var y int
 
 func main() {
 	fmt.Println(x)
 	fmt.Printf("%T\n",x)
 	x = 42
 	fmt.Println(x)
+
+	y = int(x)
+
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 
 }
