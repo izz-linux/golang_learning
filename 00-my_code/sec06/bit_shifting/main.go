5ca171c2172b63e48088debaf0bea2d52d82f06e
Izz Noland <izz@shipt.com>
2021-10-06 15:57:34 -0500

    finished section 6

---
+++ 00-my_code/sec06/bit_shifting/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+const (
+	_ =  iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+
+)
+
+func main() {
+	/*
+	x := 4
+	fmt.Printf("%d\t\t%b\n", x, x)
+
+	y := x << 1
+	fmt.Printf("%d\t\t%b", y, y)
+	*/
+
+	/*
+	kb := 1024
+	mb := 1024 * kb
+	gb := 1024 * mb
+
+
+	*/
+	fmt.Printf("%d\t\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t\t%b\n", gb, gb)
+
+
+
+}
